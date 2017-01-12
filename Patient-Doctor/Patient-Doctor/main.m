9de8a37c5ec27aa8fa57a64a245f5cd16899ea1b
alexbearinger <alexbearinger@Alexs-MacBook-Pro.local>
2017-01-12 10:40:13 -0500

    initial commit

---
+++ Patient-Doctor/Patient-Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient-Doctor
+//
+//  Created by Alex Bearinger on 2017-01-12.
+//  Copyright © 2017 Alex Bearinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
